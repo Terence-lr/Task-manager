58b647b80ae18e97151b83e21dd5aab5c9138076
Terence-lr <trichardson0826@gmail.com>
2024-12-05 21:44:53 -0500

    integrate

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const { v4: uuidv4 } = require('uuid')
+
+const taskRoutes = require('./routes/tasks') // Task routes
+const userRoutes = require('./routes/users') // User routes
+const projectRoutes = require('./routes/projects') // Project routes
 
 const app = express()
 const PORT = 3000
 
-// Middleware
-app.use(bodyParser.json()) // For JSON requests
-app.use(bodyParser.urlencoded({ extended: true })) // For form submissions
-app.use(express.static('public')) // For serving static files
-app.set('view engine', 'ejs') // Template engine
+// Middleware for parsing JSON and form data
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-// Custom Middleware
+// Serve static files (CSS, images, etc.)
+app.use(express.static('public'))
+
+// Set EJS as the template engine
+app.set('view engine', 'ejs')
+
+// Custom middleware to log requests
 app.use((req, res, next) => {
   console.log(`${req.method} request to ${req.url}`)
   next()
 })
 
+// Custom middleware to validate POST requests
 app.use((req, res, next) => {
   if (req.method === 'POST' && !req.body.description) {
     return res.status(400).send('Description cannot be empty')
@@ -24,16 +32,12 @@ app.use((req, res, next) => {
   next()
 })
 
-// Routes
-const taskRoutes = require('./routes/tasks')
-const userRoutes = require('./routes/users')
-const projectRoutes = require('./routes/projects')
+// Route handlers
+app.use('/tasks', taskRoutes) // Tasks routes
+app.use('/users', userRoutes) // Users routes
+app.use('/projects', projectRoutes) // Projects routes
 
-app.use('/tasks', taskRoutes)
-app.use('/users', userRoutes)
-app.use('/projects', projectRoutes)
-
-// Error Handling Middleware
+// Error-handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack)
   res.status(500).render('error', { message: 'Something went wrong!' })
@@ -41,5 +45,5 @@ app.use((err, req, res, next) => {
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
+  console.log(`Server is running on http://localhost:${PORT}`)
 })

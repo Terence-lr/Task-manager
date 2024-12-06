c7c9bee8e57a42c8a02df4c7ba12311f589a8690
Terence-lr <trichardson0826@gmail.com>
2024-12-05 22:16:54 -0500

    fix

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
+// Import routes
 const taskRoutes = require('./routes/tasks')
 const userRoutes = require('./routes/users')
 const projectRoutes = require('./routes/projects')
@@ -9,18 +11,31 @@ const app = express()
 const PORT = 3000
 
 // Middleware
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static('public'))
-app.set('view engine', 'ejs')
+app.use(bodyParser.json()) // Parse JSON request bodies
+app.use(bodyParser.urlencoded({ extended: true })) // Parse form data
+app.use(express.static('public')) // Serve static files (e.g., index.html, styles.css)
+app.set('view engine', 'ejs') // Set EJS as the template engine
 
-// Custom Middleware
+// Override methods for DELETE/PATCH in forms
+app.use(methodOverride('_method'))
+
+// Custom middleware to log requests
 app.use((req, res, next) => {
   console.log(`${req.method} request to ${req.url}`)
   next()
 })
 
-// Route Handlers
+// In-memory data for rendering the EJS views
+let tasks = []
+let users = []
+let projects = []
+
+// Home route to render EJS views
+app.get('/', (req, res) => {
+  res.render('index', { tasks, users, projects })
+})
+
+// Routes
 app.use('/tasks', taskRoutes)
 app.use('/users', userRoutes)
 app.use('/projects', projectRoutes)
@@ -29,6 +44,11 @@ app.use('/projects', projectRoutes)
 app.use((err, req, res, next) => {
   console.error(err.stack)
   res.status(500).render('error', { message: 'Something went wrong!' })
+})
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`)
 })
 
 // Start Server
